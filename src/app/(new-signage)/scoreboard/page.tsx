707845c14e78f679da48df4d442632bbc1f6d688
Minh Duong <git@whitehoodhacker.net>
2023-09-20 01:20:02 -0500

    more wip

---
+++ src/app/(new-signage)/scoreboard/page.tsx
@@ -0,0 +1,29 @@
+"use client";
+import Scoreboard from "@/components/Scoreboard";
+
+export default function ScoreboardPage() {
+  return (
+    <div className="grid grid-cols-2 gap-2 p-2">
+      <div className="flex flex-col">
+        <span className="font-medium text-2xl">
+          Advanced Division
+        </span>
+        <Scoreboard
+          ctfd_url="http://signage-api.sigpwny.com/"
+          matched={true}
+          limit={5}
+        />
+      </div>
+      <div className="flex flex-col">
+        <span className="font-medium text-2xl">
+          Beginner Division
+        </span>
+        <Scoreboard
+          ctfd_url="http://signage-api.sigpwny.com/"
+          matched={false}
+          limit={5}
+        />
+      </div>
+    </div>
+  );
+}
--- src/components/Scoreboard/index.tsx
+++ src/components/Scoreboard/index.tsx
@@ -5,6 +5,7 @@ interface CTFdScoreboardUserTeam {
   account_id: number;
   account_type: string;
   name: string;
+  score: number;
   pos: number;
 }
 
@@ -16,7 +17,59 @@ interface ScoreboardProps {
 
 export default function Scoreboard(data: ScoreboardProps) {
   const { ctfd_url, matched, limit } = data;
-  const [scoreboard, setScoreboard] = useState<CTFdScoreboardUserTeam[]>([]);
+  // Test data
+  const test_scoreboard = [
+    {
+      account_id: 1,
+      account_type: "user",
+      name: "thisisareallylongnamethatshouldntfitinthescoreboardbutitdoes",
+      score: 100,
+      pos: 1,
+    },
+    {
+      account_id: 2,
+      account_type: "user",
+      name: "test2",
+      score: 50,
+      pos: 2,
+    },
+    {
+      account_id: 3,
+      account_type: "user",
+      name: "test3",
+      score: 25,
+      pos: 3,
+    },
+    {
+      account_id: 4,
+      account_type: "user",
+      name: "test4",
+      score: 10,
+      pos: 4,
+    },
+    {
+      account_id: 5,
+      account_type: "user",
+      name: "test5",
+      score: 5,
+      pos: 5,
+    },
+    {
+      account_id: 6,
+      account_type: "user",
+      name: "test6",
+      score: 1,
+      pos: 6,
+    },
+    {
+      account_id: 7,
+      account_type: "user",
+      name: "test7",
+      score: 0,
+      pos: 7,
+    }
+  ];
+  const [scoreboard, setScoreboard] = useState<CTFdScoreboardUserTeam[]>(test_scoreboard);
   // Initialize split scoreboard from the CTFd API
   useEffect(() => {
     async function fetchScoreboard() {
@@ -27,30 +80,34 @@ export default function Scoreboard(data: ScoreboardProps) {
       if (!data.success || !data.data || !data.data.matched || !data.data.unmatched) return;
       console.log(data.data)
       // Get first 5 users from the matched or unmatched scoreboard, or all if limit is not set
-      const scoreboard = matched ? data.data.matched : data.data.unmatched;
-      const scoreboard_limit = limit ? limit : scoreboard.length;
-      setScoreboard(scoreboard.slice(0, scoreboard_limit));
+      // const scoreboard = matched ? data.data.matched : data.data.unmatched;
+      // const scoreboard_limit = limit ? limit : scoreboard.length;
+      // setScoreboard(scoreboard.slice(0, scoreboard_limit));
     }
-    fetchScoreboard();
+    // fetchScoreboard();
   }, []);
+  const display_limit = limit ? limit : scoreboard.length;
   return (
-    <div className="flex flex-col">
+    <div className="flex flex-col gap-1 w-full">
       {scoreboard.length != 0 ? (
         <>
-          {scoreboard.map((user, idx) => (
-            <div key={idx} className="flex flex-row">
-              <span className="flex flex-col w-[5ch] font-medium font-mono text-[3rem] leading-none">
+          {scoreboard.filter((user, idx) => idx < display_limit).map((user, idx) => (
+            <div key={idx} className="flex flex-row bg-surface rounded-lg p-1">
+              <span className="flex flex-col w-[5ch] font-mono">
                 {idx + 1}
               </span>
-              <span className="flex flex-col font-medium font-mono text-[3rem] leading-none">
+              <span className="flex flex-col font-mono">
                 {user.name}
+              </span>
+              <span className="flex flex-col w-[5ch] font-mono">
+                {user.score}
               </span>
             </div>
           ))}
         </>
       ) : (
         <div className="flex flex-row">
-          <span className="font-medium text-[3rem] leading-none">
+          <span className="font-medium leading-none">
             Scoreboard is currently hidden
           </span>
         </div>
